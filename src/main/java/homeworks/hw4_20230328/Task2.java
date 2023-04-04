4f191fa7a0d72b9fad33a2421f3ceecee10d445f
o.demyanenko <olgadem@hotmail.com>
2023-04-04 09:52:17 +0300

    hw4

--- src/main/java/homeworks/hw4_20230328/Task2.java
+++ src/main/java/homeworks/hw4_20230328/Task2.java
@@ -10,8 +10,55 @@ package homeworks.hw4_20230328;
 32 также можно выбрать в качестве пика.
 */
 
+import java.util.Arrays;
+
 public class Task2 {
   public static void main(String[] args) {
+    int[][] array = {
+            {10, 20, 15},
+            {21, 30, 14},
+            {7, 16, 32}
+    };
 
+    printArray(array);
+
+    int peakElement = getPeakElement(array);
+    System.out.println("Peak element: " + peakElement);
+
+  }
+
+  private static int getPeakElement(int[][] array) {
+    if (array.length == 0) {
+      return -1;
+    }
+    int peakElem = -1;
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array.length; j++) {
+        if (
+                ((j == 0) && (array[i][j] >= array[i][j + 1])) ||
+                ((j == array.length - 1) && (array[i][j] >= array[i][j - 1])) ||
+                ((j > 0 && j < array.length - 1) && (array[i][j] >= array[i][j - 1]) && (array[i][j] >= array[i][j + 1])) ||
+
+                ((i == 0) && (array[i][j] >= array[i + 1][j])) ||
+                ((i == array.length - 1) && (array[i-1][j] >= array[i][j])) ||
+                ((i > 0 && i < array.length - 1) && (array[i-1][j] >= array[i][j]) && (array[i+1][j] >= array[i][j]))
+
+        ) {
+          if (peakElem < array[i][j]) {
+            peakElem =  array[i][j];
+          }
+        }
+      }
+    }
+    return peakElem;
+  }
+
+  private static void printArray(int[][] array) {
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array[i].length; j++) {
+        System.out.print(array[i][j] + "\t");
+      }
+      System.out.println();
+    }
   }
 }
